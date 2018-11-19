17918e4739814e08e480ee8c2563cf7792d08873
kaluolin <3531710594@qq.com>
2018-11-19 17:02:35 +0800

    二次修改

--- src/git_demo/HelloWorld.java
+++ src/git_demo/HelloWorld.java
@@ -3,7 +3,15 @@ package git_demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 11; j++) {
+				if (j>i) {
+					continue;
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 
 	}
 
